e1d6345e112a9cf8f8f0d117f89cf6f7c06b7a2c
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-27 17:34:38 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   
    def create
      binding.pry
-    @user = User.new(user_params).save
+    @user = User.create(user_params)
     session[:user_id] = @user.id
     redirect_to "/users/#{@user.id}"
   end
