6237cd9a07ea457326c005ccd0065099321ff18d
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-27 17:27:21 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,6 @@ class SessionsController < ApplicationController
     @user = User.find_by(name: params[:user][:name])
     redirect_to "/users/login" unless @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
+    redirect
   end
 end
