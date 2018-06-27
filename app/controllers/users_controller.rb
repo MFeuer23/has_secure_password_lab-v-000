8d0e1e28bbd1f716e8ceba7e78b8c3a989bcc450
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-27 17:38:36 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,10 @@ class UsersController < ApplicationController
   
    def create
 
-    @user = User.create(user_params)
-    session[:user_id] = @user.id
-    redirect_to "/users/#{@user.id}"
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to "/users/#{@user.id}"
   end
  
   private
