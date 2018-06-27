d2115e12188dd12f56e324cfb305283fc11f93cc
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-27 17:26:20 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     binding.pry
     @user = User.find_by(name: params[:user][:name])
-    redirect_to "/users/#{@user.id}" unless @user.authenticate(params[:user][:password])
+    redirect_to "/users/login" unless @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
   end
 end
