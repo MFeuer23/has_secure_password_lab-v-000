d9b213cbb7262f7c91ff84a2847c2cd428c768c8
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-27 16:00:56 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,19 @@ class UsersController < ApplicationController
   def new
   end
 
-  def create
-  end
   
   def show
   end
+  
+   def create
+    user = User.new(user_params).save
+  end
+ 
+  private
+ 
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+  
   
 end
