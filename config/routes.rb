c09f8d38a03ff346ac1abafc6ffbd335750d659d
Mallory Feuer <mallory.feuer@gmail.com>
2018-06-27 16:07:20 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,14 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-     get 'users/signup' => 'user#new'
-     post '/create' => 'user#create'
+   #  get 'users/signup' => 'user#new'
+  #   post '/create' => 'user#create'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+     resources :users
 
   # Example resource route with options:
   #   resources :products do
